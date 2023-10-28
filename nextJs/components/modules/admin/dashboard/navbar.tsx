d88aea05a668dd20c8518b9ba6437eae80e88508
A-AllaaEddine <a.allaaeddine.riad@gmail.com>
2023-10-28 20:01:55 +0800

    reverted back to nextjs 13.5.6

--- nextJs/components/modules/admin/dashboard/navbar.tsx
+++ nextJs/components/modules/admin/dashboard/navbar.tsx
@@ -35,9 +35,10 @@ const NavBar = () => {
   ];
   return (
     <div className="w-44 h-auto  flex flex-col justify-start items-start  font-[500] gap-2">
-      {routes.map(({ href, label, active }) => {
+      {routes.map(({ href, label, active }, index) => {
         return (
           <Link
+            key={index}
             href={href}
             className={`text-start w-full h-10 rounded-md p-2  hover:bg-slate-100
                 ${active ? 'bg-slate-100' : 'bg-white'}`}
